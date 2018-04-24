f05a5ac7f76887522a0166865287e5d5bea3e4ac
Chappell M <Chappell@Chappells-MacBook-Pro.local>
2018-04-24 02:32:50 -0400

    ParseChat done

---
+++ ParseChat/MessageCell.swift
@@ -0,0 +1,25 @@
+//
+//  MessageCell.swift
+//  ParseChat
+//
+//  Created by Collin S. on 4/8/2018.
+//  Copyright © 2018 Collin S. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UICollectionViewCell {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+
+ 
+
+}
